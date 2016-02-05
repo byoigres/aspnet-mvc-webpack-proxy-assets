3f2a401f776b0126f5684931a5895d69dc90f391
Sergio Flores <e-saflores@neoris.com>
2016-02-04 17:34:39 -0700

    Added HMR, SASS and production build

--- Content/webpack.config.js
+++ Content/webpack.config.js
@@ -1,12 +1,20 @@
 var webpack = require('webpack');
+var extractTextPlugin = require('extract-text-webpack-plugin');
+var clean = require('clean-webpack-plugin');
+var webpackStrip = require('strip-loader');
+var path = require('path');
 var IS_DEBUG = process.env.NODE_ENV !== 'production';
 
 var webpackConfig = {
   entry: {
-    main: [ './js/index.js' ],
+    home: [
+      './src/entries/home',
+      './src/styles/entries/home'
+    ],
     vendor: [
       // Styles
       'bootstrap/dist/css/bootstrap',
+      './src/styles/core',
       // Scripts
       'bootstrap',
       'jquery'
@@ -18,16 +26,35 @@ var webpackConfig = {
     publicPath: '/Content/bundles/'
   },
   resolve: {
-    extensions: ['', '.js', '.css']
+    extensions: ['', '.js', '.css', '.scss']
   },
   module: {
     loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/,
+        cacheable: true,
+        query: {
+          presets: ['es2015'],
+          retainLines: true,
+          cacheDirectory: true
+        }
+      },
+      { test: /\.js$/, loader: 'eslint-loader', exclude: /(node_modules|bower_components)/ },
       { test: /\.css$/, loaders: ['style', 'css'] },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
       { test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
+  },
+  sassLoader: {
+    includePaths: ['./src/styles'],
   },
   devtool: IS_DEBUG ? 'cheap-source-map' : 'source-map',
   plugins: [
@@ -55,6 +82,35 @@ if (IS_DEBUG) {
       webpackConfig.entry[item].unshift('webpack-hot-middleware/client');
     }
   });
+}
+
+// Production
+if (!IS_DEBUG) {
+  webpackConfig.plugins.push(
+    new clean(['bundles']),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new extractTextPlugin("[name].css")
+  );
+
+  webpackConfig.module.loaders.forEach(function(item) {
+    if (item.test.test('.scss')) {
+      delete item.loaders;
+      item.loader = extractTextPlugin.extract('css!sass');
+    }
+
+    if (item.test.test('.css')) {
+      delete item.loaders;
+      item.loader = extractTextPlugin.extract('css')
+    }
+  });
+
+  webpackConfig.module.loaders.unshift(
+    { test: /\.js$/, loader: webpackStrip.loader('debug', 'console.log') }
+  );
 }
 
 module.exports = webpackConfig;
